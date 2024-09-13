8def29777b408f04be29eea75657d269b829609c
vigo walker <vigopaul05@gmail.com>
2024-09-13 10:20:15 -0300

    fesfs

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
 from pocketoptionapi.utils.login import Login
 
 lg = Login()
-lg.login(username="vigopaul05@gmail.com", password="lagunaVerde03")
+lg.login(username="vigopaul05@gmail.com", password="123456")
