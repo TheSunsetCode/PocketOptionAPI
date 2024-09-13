e822febf5a1d81c05d101b3a967c728d295f6e69
vigo walker <vigopaul05@gmail.com>
2024-09-13 10:18:09 -0300

    dwawad

---
+++ pocketoptionapi/utils/login.py
@@ -0,0 +1,10 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Chrome
+from selenium.webdriver.ie.service import Service
+
+class Login:
+    def __init__(self) -> None:
+        self.service = Service(executable_path=ChromeDriverManager().install())
+        self.driver = Chrome(service=self.service)
+    def login(self, username, password, url: str = "https://pocketoption.com/en/login"):
+        self.driver.get(url)
